aa8f3482fbe20e3687f34ca8b8e763182cde0f85
skramyah <104219582+skramyah@users.noreply.github.com>
2023-08-22 19:22:20 +0530

    Add files via upload

---
+++ assignments/Bike.java
@@ -0,0 +1,14 @@
+package week1.day1.assignments;
+
+public  class Bike {
+	public static void main(String[] args){
+		
+		
+		Car c=new Car();
+		c.applyBreak();
+		c.soundHorn();
+		Bike b =new Bike();
+		
+	}
+
+}
